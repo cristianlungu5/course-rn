f0141870d75bd972d89830a0fe85bb5fdb5f3839
Cristian Lungu <Cristian.Lungu@endava.com>
2020-04-21 15:49:01 +0300

    Lesson 6 -  button and touchable opacity.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>React Native</Text>
-  </View>
-);
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native</Text>
+      <Button
+      onPress={() => navigation.navigate('ComponentsScreen')}
+      title="Go to demo components" />
+      <Text style={styles.title}>Touchable</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('ListsScreen')}>
+        <Text>Go to Lists</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
